a7977119a190575965830c11c30da8b47716ade2
Mariia Horiunova <mariya.vivchar@gmail.com>
2024-08-08 23:07:49 +0300

    Added task files in jsBasics

---
+++ jsBasics/task1.js
@@ -0,0 +1,14 @@
+let myString = "Hello, world!";
+console.log("String:", myString);
+
+let myNumber = 42;
+console.log("Number:", myNumber);
+
+let myBoolean = true;
+console.log("Boolean:", myBoolean);
+
+let myNull = null;
+console.log("Null:", myNull);
+
+let myUndefined;
+console.log("Undefined:", myUndefined);
